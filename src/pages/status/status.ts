46e61f7af86853f1ef78d8a057db96c3e1bdaa84
mkenirons <martin.kenirons@gmit.ie>
2018-04-07 22:08:48 +0100

    first commit

--- src/pages/status/status.ts
+++ src/pages/status/status.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Storage} from '@ionic/storage';
 /**
- * Generated class for the HomePage page.
+ * Generated class for the StatusPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,24 +10,25 @@ import {Storage} from '@ionic/storage';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-status',
+  templateUrl: 'status.html',
 })
-export class HomePage {
+export class StatusPage {
 
 myStatus: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  constructor(public navCtrl: NavController, private storage:Storage, public navParams: NavParams) {
+  }
+
+  saveStatus(){
+    this.storage.set("myStatus", this.myStatus);
+    this.navCtrl.pop();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad StatusPage');
   }
 
-  updateStatus(){
-    this.navCtrl.push("StatusPage");
-  }
-
-   ionViewWillEnter(){
+ionViewWillEnter(){
     this.storage.get("myStatus")
     .then((data) =>
     {
@@ -37,5 +38,4 @@ myStatus: string;
       alert("Error accesssing Storage")
     })
   }
-
 }
